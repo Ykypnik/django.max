5088615881da0b7f84aa3226cbdd740d1ced4357
Egor <123@gmail.com>
2023-07-18 15:24:02 +0500

    djinja

--- app_advertisements/urls.py
+++ app_advertisements/urls.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-from .views import index
+from .views import index, top_sellers
 from django.urls import path
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='main-page'),
+    path('top-sellers', top_sellers, name='top-sellers')
 ]
